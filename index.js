97a5fd101f746ae34a4ee3f7cc77b4ad85ed9422
Joo Hyun Kim <stevekim1109@gmail.com>
2020-09-13 03:00:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  for (let i = 0; i < musicians.length; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return empty;
+}
+
+function johnLennonFacts(facts) {
+  while ()
+}
