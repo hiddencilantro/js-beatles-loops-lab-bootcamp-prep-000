a47203ccac09868014764eca19a5a61f06d5f3ed
Joo Hyun Kim <stevekim1109@gmail.com>
2020-09-13 03:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ function theBeatlesPlay(musicians, instruments) {
 
 let i = 0;
 
+function increment() {
+  i += 1;
+}
+
 function johnLennonFacts(facts) {
   var newFacts = [];
   while (newFacts.length < facts.length) {
-    newFacts.push(`${facts[i]}!!!`)
+    newFacts.push(`${facts[increment()]}!!!`)
   }
   return newFacts;
 }
